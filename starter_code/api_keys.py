ee57f5905ad937cbd5bf2c8e35b9340a62685674
gary castillo <garycastle@garys-MacBook-Pro.local>
2021-04-24 15:00:53 -0700

    Working

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9157a5f1257edba8424bfe55df35ce1f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDMMvPIxl3b7LL_XkpcjyKN8gsiSo5beVs"
